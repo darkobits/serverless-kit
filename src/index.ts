8e8d3d2ab2c21cb633a8b9948643072096be8d66
Joshua <joshua+github@frontlawn.net>
2023-06-10 05:24:30 -0700

    fix: Update root package exports.

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export { httpErrorHandlerMiddleware } from 'lib/error-handler';
 export { logger, tracer, metrics } from 'lib/powertools';
 export { res } from 'lib/response';
 export { getSecret } from 'lib/sst';
-export { jsonToBase64, base64ToJson } from 'lib/utils';
+export { jsonToBase64, base64ToJson, getLocalStageName } from 'lib/utils';
