23e984351f63ab12e2e4780d6c84bdccad4f1372
Joshua <joshua+github@frontlawn.net>
2024-02-14 23:26:50 -0800

    chore(deps): Update dependencies.

--- nr.config.js
+++ nr.config.js
@@ -1,9 +1,1 @@
-import { nr } from '@darkobits/ts';
-
-
-/**
- * See: https://github.com/darkobits/nr
- */
-export default nr(({ command, task, script }) => {
-  // Add your own commands, tasks, and scripts here.
-});
+export { defaultPackageScripts as default } from '@darkobits/ts';
